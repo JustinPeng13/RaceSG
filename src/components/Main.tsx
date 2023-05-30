eaa91cfd98e756b3852bf0444d034be540850c1a
yeoyujie <yeo.yujie@gmail.com>
2023-05-30 08:51:22 +0800

    resolved merge conflicts

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -197,7 +197,7 @@ export default function Main({ children }: { children: React.ReactNode }) {
         </Disclosure>
 
         <main className="bg-white shadow">
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
             {children}
           </div>
         </main>
